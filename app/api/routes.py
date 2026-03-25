dc8200acb17e242220b535417cbd7c33f4043f99
wesker <canaldark191@gmail.com>
2026-03-25 19:14:09 -0300

    Parser and route refactoring. Regex bug fix.

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.parser import extract_data
+from app.email_reader import email_reader
 from app.models import Order
 from app.schemas import OrderSchema
 
@@ -10,3 +12,25 @@ router = APIRouter()
 @router.get("/orders", response_model=list[OrderSchema])
 def get_orders(db: Session = Depends(get_db)):
     return db.query(Order).all()
+
+
+@router.post("/process")
+def process_emails(db: Session = Depends(get_db)):
+    emails = email_reader()
+    for email in emails:
+        data = extract_data(email)
+
+        if not data:
+            continue
+
+        order = Order(
+            number=data["number"],
+            client=data["client"],
+            value=data["value"]
+        )
+
+        db.add(order)
+
+    db.commit()
+
+    return {"message": "Emails processed successfully."}
--- app/parser.py
+++ app/parser.py
@@ -1,24 +1,26 @@
 import re
 
 def extract_data(text: str) -> list[dict]:
-    lines = text.splitlines()
-    result = []
-    
-    for line in lines:
-        match = re.search(
-                r'Pedido\s+#(\d+)\s+-\s+Cliente\s+([A-Za-zÀ-ÿ\s]+?)\s+-\s+Valor\s*(.*)',
-                line
-            )
+    match = re.search(
+    r'Pedido\s+#(\d+)\s+-\s+Cliente\s+([A-Za-zÀ-ÿ\s]+?)\s+-\s+Valor\s*(.*)',
+    text
+    )
         
-        if match:
-            id, name, value = match.groups()
-            
-            value = value.strip()
+    if not match:
+        return None
 
-            if not value:
-                value = "SEM VALOR"
-            
-            
-            result.append({'id': id, 'name': name, 'value': value})
-        
-    return result
+    number, client, raw_value = match.groups()
+
+    value = re.sub(r"[^\d,\.]", "", raw_value) 
+    value = value.replace(",", ".")  
+
+    try:
+        value = float(value)
+    except:
+        return None  
+
+    return {
+        "number": int(number),
+        "client": client.strip(),
+        "value": value
+    }
